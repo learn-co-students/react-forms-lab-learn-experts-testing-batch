c52212c696f3b791d64e5f78a8acf715d812c16e
cernanb <cernanb@gmail.com>
2016-09-21 22:36:34 -0700

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,51 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    if (this.state.username !== '' && this.state.password !== '') {
+      this.props.onSubmit(this.state)
+    }
+    else {
+      console.error('Username and password cannot be empty')
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" onChange={this.handleUsernameChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" onChange={this.handlePasswordChange}/>
           </label>
         </div>
         <div>
--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -1,17 +1,53 @@
 const React = require('react');
 
+function checkPoem(poem) {
+  const strArr = poem.split('\n').filter(l => l)
+  const numOfLines = strArr.length === 3;
+  const numOfWords = checkWordCount(strArr[0]) === 5 && checkWordCount(strArr[1]) === 3 && checkWordCount(strArr[2]) === 5;
+  return numOfLines && numOfWords;
+}
+
+function checkWordCount(str) {
+  return str.split(' ').filter(l => l).length
+}
+
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      text: '',
+      isValidPoem: false
+    };
+
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(e) {
+    const str = e.target.value
+
+    if (str) {
+      this.setState({
+        text: str,
+        isValidPoem: checkPoem(str)
+      })
+    }
+
+    // this.checkPoem(this.state.text)
+    console.log(this.state.text)
+  }
+
+  // checkPoem(poem) {
+  //   const strArr = poem.split('\n')
+  //   console.log(strArr)
+  // }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.text} onChange={this.handleChange}/>
+        {!this.state.isValidPoem ? <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div> : null}
       </div>
     );
   }

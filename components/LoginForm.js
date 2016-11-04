07f5c0aff75ec04c58eee4830c25e8924637b1ef
Enoch2k2 <enoch2k2@hotmail.com>
2016-11-03 23:22:38 -0500

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,48 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.changeInputState = this.changeInputState.bind(this);
+    this.submitState = this.submitState.bind(this);
+  }
+
+  changeInputState(field, event){
+    this.setState({
+      [field]: event.target.value
+    })
+  }
+
+  submitState(ev){
+    ev.preventDefault();
+    const {username, password } = this.state;
+
+    if(!username || !password) {
+      return;
+    }
+
+    this.props.onSubmit({
+      username,
+      password
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitState}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.changeInputState.bind(this, 'username')}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.changeInputState.bind(this, 'password')}/>
           </label>
         </div>
         <div>
--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -1,17 +1,44 @@
 const React = require('react');
 
+function countWords(line){
+  return line.split(' ').filter(f => f).length;
+}
+
+function validPoem(poem){
+  var lines = poem.split('\n').filter(l => l);
+  var correctLineCount = lines.length === 3;
+  var correctWordCount = countWords(lines[0]) === 5 && countWords(lines[1]) === 3 && countWords(lines[2]) === 5;
+
+  return correctLineCount && correctWordCount;
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: '',
+      isValid: false
+    };
+
+    this.setPoemContent = this.setPoemContent.bind(this);
+  }
+
+  setPoemContent(ev){
+    const content = ev.target.value
+    if(content){
+      this.setState({
+        content: content,
+        isValid: validPoem(content)
+      })
+    }
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.content} onChange={this.setPoemContent}/>
+        {!this.state.isValid ? <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div> : null}
       </div>
     );
   }

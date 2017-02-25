baad3da948b3f9ff4db000f5790368d29cd0df22
NStephenson <nickstephenson90@gmail.com>
2017-02-24 20:16:27 -0500

    I think match is better than using split for PoemWriter

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,49 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleUsernameChange(e){
+    this.setState({username: e.target.value});
+  }
+
+  handlePasswordChange(e){
+    this.setState({password: e.target.value});
+  }
+
+  handleSubmit(e){
+    if (!this.state.username || !this.state.password){
+      e.preventDefault();
+      console.error("This form is invalid");
+    } else {
+      e.preventDefault()
+      this.props.onSubmit(e);
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit= {this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" 
+            value={this.state.username} onChange={this.handleUsernameChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" 
+            value={this.state.password} onChange={this.handlePasswordChange} />
           </label>
         </div>
         <div>
